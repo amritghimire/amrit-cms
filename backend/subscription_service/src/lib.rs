3020596158811c2b81c3a122364949d8e8b97d12
Amrit Ghimire <mail@amritghimire.com>
2024-02-23 16:22:33 +0545

    Add sqlx prepare query for tests as well

--- backend/subscription_service/src/lib.rs
+++ backend/subscription_service/src/lib.rs
@@ -8,7 +8,7 @@ mod handler;
 pub mod helper;
 pub mod router;
 
-pub async  fn migrate(pool: &PgPool){
+pub async fn migrate(pool: &PgPool) {
     sqlx::migrate!()
         .run(pool)
         .await
